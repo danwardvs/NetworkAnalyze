f8e6e9ea392de7ec82173b170258274c490f515d
Danny Van Stemp <danwardvs@adsgames.net>
2020-02-10 20:54:01 -0500

    Finished cleaning up the script version of the program

--- process.py
+++ process.py
@@ -2,110 +2,71 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values) 
+        val = int(round(pct*total/100.0))
+        return '({v:d})'.format(v=val)
+    return my_autopct
+
+def draw_graph(title,field,other_tolerance,make_other,main_bar,other_bar):
+
+    df[field].fillna('Unknown',inplace=True)
+    df[field].replace('Generic','Unknown',inplace=True)
+
+
+    makes = df[field].value_counts()
+    if make_other:
+        main = makes[makes > other_tolerance]
+        other = makes[makes<=other_tolerance]
+        main = main.append(pd.Series([other.sum()],['Other']))
+    else:
+        main = makes
+
+    if make_other:
+        explode = []
+
+        for i in range(len(main)-1):
+            explode.append(0)
+        explode.append(0.2)
+
+
+    if not main_bar:
+        main.plot.pie(autopct = make_autopct(main))
+    else:
+        plt.subplots(figsize=(8,6))
+        main.plot.bar()
+        plt.ylabel("Device Count")
+        plt.xlabel("Device "+ field.capitalize())
+
+    plt.title(title,size=16)
+    plt.ylabel("")
+    plt.tight_layout()
+    plt.show()
+
+    if make_other:
+        if not other_bar:
+            other.plot.pie(autopct = make_autopct(other))
+            plt.ylabel("")
+        else:
+            plt.subplots(figsize=(8,6))
+            other.plot.bar()
+            plt.ylabel("Device Count")
+            plt.xlabel("Device "+ field.capitalize())
+            
+
+        plt.title("Other Devices",size=16)
+        
+        plt.tight_layout()
+        plt.show()
+
 data = json.load(open('data/data.json'))
 
 header = pd.DataFrame(data['network'])
 
 df = pd.DataFrame(data["devices"])
 
-df['make'].fillna('Unknown',inplace=True)
 
-makes = df['make'].value_counts()
-main = makes[makes > 7]
-other = makes[makes<=7]
-main = main.append(pd.Series([other.sum()],['Other']))
-
-explode = []
-
-for i in range(len(main)-1):
-    explode.append(0)
-explode.append(0.2)
-
-def make_autopct(values):
-    def my_autopct(pct):
-        total = sum(values)
-        val = int(round(pct*total/100.0))
-        return '({v:d})'.format(v=val)
-    return my_autopct
-
-main.plot.pie(explode=explode, autopct = make_autopct(main))
-
-plt.title("Manufacturers of Devices",size=16)
-plt.ylabel("")
-plt.show()
-
-other.plot.pie(autopct = make_autopct(other))
-plt.title("Other Devices",size=16)
-plt.ylabel("")
-
-
-plt.show()
-
-df['model'].fillna('Unknown',inplace=True)
-
-makes = df['model'].value_counts()
-main = makes[makes > 7]
-other = makes[makes<=7]
-main = main.append(pd.Series([other.sum()],['Other']))
-
-explode = []
-
-for i in range(len(main)-1):
-    explode.append(0)
-explode.append(0.2)
-
-def make_autopct(values):
-    def my_autopct(pct):
-        total = sum(values)
-        val = int(round(pct*total/100.0))
-        return '({v:d})'.format(v=val)
-    return my_autopct
-
-main.plot.pie(explode=explode, autopct = make_autopct(main))
-
-plt.title("Models of Devices",size=16)
-plt.ylabel("")
-plt.show()
-
-plt.subplots(figsize=(10,5))
-other.plot.bar()
-
-plt.title("Other Devices",size=16)
-plt.ylabel("")
-
-
-plt.show()
-
-df['type'].fillna('Unknown',inplace=True)
-df['type'].replace('Generic','Unknown',inplace=True)
-
-makes = df['type'].value_counts()
-main = makes[makes > 22]
-other = makes[makes<=22]
-main = main.append(pd.Series([other.sum()],['Other']))
-
-explode = []
-
-for i in range(len(main)-1):
-    explode.append(0)
-explode.append(0.2)
-
-def make_autopct(values):
-    def my_autopct(pct):
-        total = sum(values)
-        val = int(round(pct*total/100.0))
-        return '({v:d})'.format(v=val)
-    return my_autopct
-
-main.plot.pie(explode=explode, autopct = make_autopct(main))
-
-plt.title("Types of Devices",size=16)
-plt.ylabel("")
-plt.show()
-
-other.plot.pie(autopct = make_autopct(other))
-plt.title("Other Devices",size=16)
-plt.ylabel("")
-
-
-plt.show()
+draw_graph("Manufacturers of Devices",'make',7,True,False,False)
+draw_graph("Types of Devices",'type',22,True,False,False)
+draw_graph("Models of Devices",'model',7,True,False,True)
