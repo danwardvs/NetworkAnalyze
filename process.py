c126848078c86b43ab0575495dc521b910e72998
Danny Van Stemp <danwardvs@adsgames.net>
2020-02-10 21:31:31 -0500

    Saving to file function is started

--- process.py
+++ process.py
@@ -2,6 +2,20 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 
+write_to_file = False
+file_name = 'output/file_'
+file_extension = '.png'
+file_index = 0
+
+def print_graph(plt):
+
+    if write_to_file:
+        plt.savefig(file_name + str(file_index) + file_extension)
+        file_index=file_index+1
+    else:
+        plt.show()
+
+
 def make_autopct(values):
     def my_autopct(pct):
         total = sum(values) 
@@ -41,14 +55,14 @@ def draw_graph(title,field,other_tolerance,make_other,main_bar,other_bar):
     plt.title(title,size=16)
     plt.ylabel("")
     plt.tight_layout()
-    plt.show()
+    print_graph(plt)
 
     if make_other:
         if not other_bar:
             other.plot.pie(autopct = make_autopct(other))
             plt.ylabel("")
         else:
-            plt.subplots(figsize=(8,6))
+            #plt.subplots(figsize=(8,6))
             other.plot.bar()
             plt.ylabel("Device Count")
             plt.xlabel("Device "+ field.capitalize())
@@ -57,15 +71,31 @@ def draw_graph(title,field,other_tolerance,make_other,main_bar,other_bar):
         plt.title("Other Devices",size=16)
         
         plt.tight_layout()
-        plt.show()
+        print_graph(plt)
 
 data = json.load(open('data/data.json'))
 
-header = pd.DataFrame(data['network'])
+network_header = pd.DataFrame(data['network'])
+internet_header = pd.DataFrame(data['internet'],index=[0])
+
 
 df = pd.DataFrame(data["devices"])
 
+name = pd.DataFrame(network_header['name']).iloc[0]['name']
+date = pd.DataFrame(network_header['last_changed']).iloc[0]['last_changed']
 
-draw_graph("Manufacturers of Devices",'make',7,False,False,False)
-draw_graph("Types of Devices",'type',22,False,False,False)
-draw_graph("Models of Devices",'model',7,False,False,True)
+
+
+provider = pd.DataFrame(internet_header['provider']).iloc[0]['provider']
+city = pd.DataFrame(internet_header['city']).iloc[0]['city']
+region = pd.DataFrame(internet_header['region']).iloc[0]['region']
+
+
+print("Analysis of devices on network \"" + name +"\" on " + date + ".")
+print("Internet provided by "+ provider+" located in " + city +", " + region +".")
+
+
+
+draw_graph("Manufacturers of Devices",'make',7,True,False,False)
+draw_graph("Types of Devices",'type',22,True,False,False)
+draw_graph("Models of Devices",'model',7,True,False,True)
